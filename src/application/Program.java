99bf1796ec5b195b89eeb2b4538637f06bd2aae4
Thiago Phelipe <phelipethiago11@gmail.com>
2024-03-26 12:23:58 -0300

    Starting to implement board and piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("hehexd");
 
 	}
 
